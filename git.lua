0b0d6fe97d889894936679732e51ce31195861e4
Astru43 <45196326+Astru43@users.noreply.github.com>
2022-05-03 02:19:19 +0300

    Added git.lua

---
+++ git.lua
@@ -0,0 +1,19 @@
+local git = "github.com/Astru43/atm6-scripts/raw/main"
+local pwd = shell.dir()
+ 
+if arg[1] == update then
+    address = "https://" .. fs.combine(git, "git.lua")
+    local res = http.get(address)
+    fs.delete("/git.lua")
+    local file = fs.open("/git.lua", w)
+    file.write(res.readAll())
+    file.clsoe()
+elseif http.checkURL("https://" .. fs.combine(git, arg[1])) then
+    address = "https://" .. fs.combine(git, arg[1])
+    print(address)
+    local res = http.get(address)
+    fs.makeDir(fs.combine(pwd, fs.getDir(arg[1])))
+    local file = fs.open(fs.combine(pwd, arg[1]), "w")
+    file.write(res.readAll())
+    file.close()
+end
