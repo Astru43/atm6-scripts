7e2caa2f33c06c78251d8d565ad4f2460b1aab9f
Astru43 <45196326+Astru43@users.noreply.github.com>
2022-05-03 05:36:18 +0300

    Added energy generation display

--- reactor/control.lua
+++ reactor/control.lua
@@ -1,19 +1,24 @@
--- control.lua v4
+-- control.lua v5
 -- Author: Astru43
 -- Date: 03/05/2022
+
 local reactor = peripheral.find("BiggerReactors_Reactor")
 local monitor = peripheral.find("monitor")
+if (reactor == nil or monitor == nil) then 
+    error "Can't find reactor or dispaly."
+ end
+
 local reactor_battery = reactor.battery()
-local capacity = reactor_battery.capacity()
+local battery_capacity = reactor_battery.capacity()
 
 peripheral.call("left", "setTextScale", 0.5)
 
 function main()
     while true do
         stored = reactor_battery.stored()
-        if capacity * 0.9998 < stored then
+        if battery_capacity * 0.9998 < stored then
             setState(false)
-        elseif capacity * 0.75 > stored then
+        elseif battery_capacity * 0.75 > stored then
             setState(true)
         end
 
@@ -30,25 +35,38 @@ function setState(state)
 end
 
 function title()
-    monitor.setCursorPos(1, 1)
-    monitor.write("control.lua v4")
+    monitor.write("control.lua v5")
+    newLine()
 end
 
-function info(stored)
+function info(stored_electricity)
     local fuel = reactor.fuelTank()
-    local fullness = stored / capacity * 100
-    monitor.setCursorPos(1, 2)
-    monitor.write(stored .. "/" .. capacity .. " : ")
-    monitor.write(tonumber(string.format("%.3f", fullness)) .. "%")
+    local fullness = stored_electricity / battery_capacity * 100
+    monitor.write(stored_electricity .. "/" .. battery_capacity .. " : ")
+    monitor.write(tonumber(string.format("%.2f", fullness)) .. "%")
+    newLine()
 
-    monitor.setCursorPos(1, 3)
     local fuel_percent = tonumber(string.format("%.2f", fuel.fuel() / fuel.capacity() * 100))
     monitor.write(fuel.fuel() .. "/" .. fuel.capacity() .. " : " .. fuel_percent)
+    newLine()
     monitor.write("Waste: " .. fuel.waste())
+    newLine()
+    
+    monitor.write(reactor_battery.producedLastTick() .. "FE")
+
+end
+
+local line = 1
+
+function newLine()
+    line = line + 1
+    monitor.setCursorPos(1, line)
 end
 
 function clear()
     monitor.clear()
+    line = 1
+    monitor.setCursorPos(1, 1)
 end
 
 main()
